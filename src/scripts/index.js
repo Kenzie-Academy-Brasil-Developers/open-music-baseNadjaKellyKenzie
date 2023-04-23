5538436d3a2d7adabfc5e7f55274dcafba7e8c56
kelly <nadjakelly2020@gmail.com>
2023-04-22 23:13:27 -0300

    terminei, obrigada meu deus

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,84 @@
 /* Desenvolva sua lógica aqui ... */
 
-// import { array } from "./theme"
+const render__ul = (array) => {
 
-// render(array)
+    const ul = document.querySelector('.render')
+    ul.innerHTML = ''
+
+    array.forEach(element => {
+
+    const li = document.createElement('li')
+
+    const img = document.createElement('img')
+    img.src = element.img;
+
+    const divInfo = document.createElement('div')
+    divInfo.classList.add('info');
+
+    const divEra = document.createElement('div')
+    divEra.classList.add('era');
+
+    const band = document.createElement('p')
+    band.innerText = element.band;
+
+    const year = document.createElement('p')
+    year.innerText = element.year;
+
+    const title = document.createElement('h3')
+    title.innerText = element.title;
+
+    const buttonPurchase = document.createElement('div')
+    buttonPurchase.classList.add('button__purchase')
+
+    const price = document.createElement('p')
+    price.innerText = `R$ ${element.price},00`
+
+    const button = document.createElement('button')
+    button.innerText = 'Comprar'
+
+    buttonPurchase.append(price,button)
+    divEra.append(band,year)
+    divInfo.append(divEra,title,buttonPurchase,)
+    li.append(img,divInfo)
+    ul.appendChild(li)
+    });
+}
+render__ul(products)
+
+
+const filter__ul = (array) => {
+  const buttons = document.querySelectorAll('#buttons__filter-music')
+  let filterButtons = []
+
+  buttons.forEach((elementButton) => {
+    elementButton.addEventListener('click', (event) => {
+      const searchCategory = categories.indexOf(event.target.innerText.trim())
+      if (searchCategory === 0) {
+        filterButtons = products
+      } else {
+        filterButtons = array.filter((fil) => {
+          return fil.category == searchCategory
+        })
+      }
+      render__ul(filterButtons)
+      
+    })
+  })
+}
+
+ filter__ul(products) 
+
+ const filter__value = (array) => {
+  const inputRage = document.querySelector('#inputRage')
+  const inputSpan = document.querySelector('.spanRage')
+  inputRage.value = 100
+  inputRage.addEventListener('input', () => {
+    const values = parseFloat(inputRage.value).toFixed(2)
+    inputSpan.textContent = `Até R$ ${values}`
+    const inputFilter = array.filter(filter => filter.price <= values)
+    render__ul(inputFilter)
+    })
+  }
+
+ filter__value(products)
+ 
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,22 +1,36 @@
 /* Desenvolva sua lógica aqui ... */
 const render = (array) => {
-    const darkButton = document.querySelector('.dark__mode');
+  const darkButton = document.querySelector('.dark__mode');
 
-    const html = document.querySelector('html');
+  const html = document.querySelector('html');
 
-    const img = document.createElement('img')
-    img.src = './src/assets/img/moon.svg'
-    const imgSun = document.createElement('img')
-    imgSun.src = './src/assets/img/sun.svg'
-    darkButton.appendChild(img)
-    
-    darkButton.addEventListener('click', () => {
-      html.classList.toggle('light-mode')
-      if (html.classList.contains('light-mode')) {
-        img.setAttribute('src', './src/assets/img/sun.svg')
-      } else {
-        img.setAttribute('src', './src/assets/img/moon.svg')
-      }
-    })
+  const img = document.createElement('img');
+  img.src = './src/assets/img/moon.svg';
+  const imgSun = document.createElement('img');
+  imgSun.src = './src/assets/img/sun.svg';
+  darkButton.appendChild(img);
+
+  const modePreference = JSON.parse(localStorage.getItem('light-mode'));
+  if (modePreference === false) {
+    img.setAttribute('src', './src/assets/img/sun.svg');
+    html.classList.add('light-mode');
+  } else {
+    img.setAttribute('src', './src/assets/img/moon.svg');
+    html.classList.remove('light-mode');
   }
-  render()
+
+  darkButton.addEventListener('click', () => {
+    html.classList.toggle('light-mode');
+    if (html.classList.contains('light-mode')) {
+      img.setAttribute('src', './src/assets/img/sun.svg');
+      localStorage.setItem('light-mode', false);
+    } else {
+      img.setAttribute('src', './src/assets/img/moon.svg');
+      localStorage.setItem('light-mode', true);
+    }
+  });
+};
+
+render();
+
+  
